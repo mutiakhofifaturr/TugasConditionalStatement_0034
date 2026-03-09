d1537211470e788d6518a5bf1225a0cda5b5aaaf
mimyutt <mutiakhofifaturr@gmail.com>
2026-03-09 22:52:38 +0700

    first commit

---
+++ conditionalstatement.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+float berat, tinggi; 
+
+void inputData()
+ {
+
+    cout << "Masukkan Berat Badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan Tinggi Badan (m): ";
+    cin >> tinggi;
+
+}
+
+float hitungBMI (float a, float b)
+{
+    return a / (b * b);
+
+}
+
+string statusBMI (float bmi)
+{
+    if (bmi < 18.5)
+        return "Berat badan kurang";
+    else if (bmi < 25)
+        return "Berat badan normal";
+    else if (bmi < 30)
+        return "Berat badan kelebihan";
+    else 
+        return "Obesitas";
+        
+
+}
+
+int main() 
+{
+    inputData();
+
+    float bmi = hitungBMI (berat, tinggi);
+
+    cout << "\nNilai BMI : " << bmi << endl;
+    cout << "Status    : " << statusBMI(bmi) << endl;
+
+    return 0;
+
+
+}
